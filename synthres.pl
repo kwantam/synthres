e4fac14236ea13db6df821abb1bed379c6aeecf0
kwantam <kwantam@gmail.com>
2011-06-10 23:03:14 +0000

    updated synthres.pl to track synthres.hs changes
    - synthres.pl now uses same termination criteria
      as the Haskell version; synthres.pl should
      produce identical results to synthBasic from
      synthres.hs
    
    
    git-svn-id: https://svn.jfet.org/rsw/src/synthres@560 e009f53e-6bd9-0310-94dc-a54994e6c07e

--- synthres.pl
+++ synthres.pl
@@ -17,57 +17,57 @@ my $precision = $ARGV[2] || 1e-6;
 
 unless ($unit && $res)
 {
-        print "Usage:\n$0 <unit> <resistance> [precision]\n\tunit and resistance are required, precision defaults to 1e-6\n";
-        exit(-1);
+    print "Usage:\n$0 <unit> <resistance> [precision]\n\tunit and resistance are required, precision defaults to 1e-6\n";
+    exit(-1);
 }
 
 sub calcres()
 {
-	my $unit = $_[0];
-	my $res  = $_[1];
-	my $prec = $_[2] || $precision;
-	my @result;
+    my $unit = $_[0];
+    my $res  = $_[1];
+    my $prec = $_[2] || $precision;
+    my @result;
 
-	return undef unless ($unit && $res);
+    return undef unless ($unit && $res);
 
-	my $norm = $res/$unit;
+    my $norm = $res/$unit;
 
-	for ($i=0;;$i++)
-	{
-        	$whole = int($norm);
-        	$norm -= $whole;
-		push @result, $whole;
-	
-        	last if ($norm<$prec || $i>10);
-		
-		# we always want precision referred to the total resistance,
-		# so it gets bigger as we start synthesizing smaller pieces
-		$prec = $prec / $norm;
-	
-        	$norm = 1/$norm;
-	}
-	
-	return \@result;
+    for ($i=0;;$i++)
+    {
+        $whole = int($norm+$prec);
+        $norm = abs($norm - $whole);
+        push @result, $whole;
+
+        last if ($norm<=$prec);
+
+        # we always want precision referred to the total resistance,
+        # so it gets bigger as we start synthesizing smaller pieces
+        $prec = $prec * ($norm + $whole) / $norm;
+
+        $norm = 1/$norm;
+    }
+
+    return \@result;
 }
 
 sub printres()
 {
-	my @resistors = @{$_[0]};
-	my $restot;
-	return undef unless defined(@resistors);
+    my @resistors = @{$_[0]};
+    my $restot;
+    return undef unless @resistors;
 
-	for ($i=0;;$i++)
-	{
-		$restot += $resistors[$i];
+    for ($i=0;;$i++)
+    {
+        $restot += $resistors[$i];
 
-		print "(" . ($i%2?"1/":"") . $resistors[$i] . "x";
-		
-		last if $i==$#resistors;
+        print "(" . ($i%2?"1/":"") . $resistors[$i] . "x";
 
-		print ($i%2?"||":"+");
-	}
+        last if $i==$#resistors;
 
-	print ")"x($i+1) . " (" . $restot . " units)\n";
+        print ($i%2?"||":"+");
+    }
+
+    print ")"x($i+1) . " (" . $restot . " units)\n";
 }
 
 &printres(&calcres($unit,$res));
